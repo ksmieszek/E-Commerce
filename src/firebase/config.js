ed6c80dff44a3fc70f4eab2b2ae02b84a98561c9
ksmieszek <ksmieszek.kontakt@gmail.com>
2022-01-18 18:47:41 +0100

    Updated firebase and git config

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAct-1GmE_vz9AXOn07YYGQbDFp_Dby0cM",
-  authDomain: "test-3632b.firebaseapp.com",
-  projectId: "test-3632b",
-  storageBucket: "test-3632b.appspot.com",
-  messagingSenderId: "809629366936",
-  appId: "1:809629366936:web:d648747110cb63c8992eed",
-  measurementId: "G-L3F2BMJ3TV",
-};
